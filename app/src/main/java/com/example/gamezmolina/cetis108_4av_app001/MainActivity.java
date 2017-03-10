428273b2efbbaa0db74f7cb9c5ef0ab42ad1d263
GAMEZ MOLINA <15325061080172@cetis108.edu.mx>
2017-03-10 14:29:45 -0700

    Initial commit

---
+++ app/src/main/java/com/example/gamezmolina/cetis108_4av_app001/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.gamezmolina.cetis108_4av_app001;
+
+            import android.content.DialogInterface;
+            import android.support.v7.app.AppCompatActivity;
+            import android.os.Bundle;
+            import android.view.View;
+            import android.widget.Button;
+            import android.widget.EditText;
+            import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity  {
+    EditText edText;
+    TextView txtview;
+    Button btn;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edText=(EditText) findViewById(R.id.Ingresar);
+        btn=(Button) findViewById(R. id.Continuar);
+        txtview=(TextView) findViewById(R.id.resultado);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String palabra= edText.getEditableText().toString();
+                txtview.setText(palabra);
+
+            }
+        });
+    }
+}
+
